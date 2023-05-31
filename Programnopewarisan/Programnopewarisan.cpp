d858683b4a5ff3c4a65c7b79fba3a00e8afaa083
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-31 20:11:22 +0700

    commit 16

--- Programnopewarisan/Programnopewarisan.cpp
+++ Programnopewarisan/Programnopewarisan.cpp
@@ -1,20 +1,17 @@
-// Programnopewarisan.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class baseClass final {
+public:
+	virtual void perkenalan() {
+		cout << "Hallo saya Function dari base class";
+	}
+};
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
