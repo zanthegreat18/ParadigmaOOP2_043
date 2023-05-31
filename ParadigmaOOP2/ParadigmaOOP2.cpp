fc1d4aebfe4d49ab44b41a29de4bd7de008b35a7
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-31 19:31:19 +0700

    commit 2

--- ParadigmaOOP2/ParadigmaOOP2.cpp
+++ ParadigmaOOP2/ParadigmaOOP2.cpp
@@ -11,3 +11,14 @@ public:
 		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
 	}
 };
+
+class pekerja : public orang {
+public:
+
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
+
